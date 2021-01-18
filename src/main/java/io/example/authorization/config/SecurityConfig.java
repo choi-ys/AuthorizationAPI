71be18bce713db26f024d701137e6634024db360
choi-ys <rcn115@naver.com>
2021-01-18 18:04:25 +0900

    [2021-01-18]용석:InMemory 환경에서 Authorization Code 방식의 인증 토큰 발급
     - pom.xml : OAuth2 방식의 API 권한/인가 Token발급을 위한 spring-security-oauth2-autoconfigure 의존성 추가
     - AuthorizationConfig.java : 인증된 사용자와 명시된 Client 정보를 이용한 authorization code방식의 Token 발급 설정 추가
     - SecurityConfig.java : InMemory환경에 인증이 허용된 사용자 정보 설정
     - Authorization Code 방식의 토큰 발급 Test를 위한 Client 토큰발급 요청/수신 부 구현
       - ApplicationConfig.java : 인증 서버와 통신을 위한 RestTemplate 등록
       - AuthToken.java : 발급받은 Token 데이터 수신을 위한 객체 구현
       - TokenReceptionController : 인증서버에 Authorization Code 요청 및 Token 발급 요청 부

--- src/main/java/io/example/authorization/config/SecurityConfig.java
+++ src/main/java/io/example/authorization/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.example.authorization.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests().antMatchers("/oauth/**", "/oauth/callback", "/h2-console/*").permitAll()
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("choi-ys")
+                .password("{noop}password")
+                .roles("USER")
+        ;
     }
 }
