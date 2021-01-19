cc8ae7c545eb7f45dd1858ab2a373aa7d57b52ff
choi-ys <rcn115@naver.com>
2021-01-19 14:58:13 +0900

    [2021-01-19]용석:Refresh Token을 이용한 Access Token 갱신 Test 코드 추가

--- src/main/java/io/example/authorization/controller/TokenReceptionController.java
+++ src/main/java/io/example/authorization/controller/TokenReceptionController.java
@@ -47,4 +47,28 @@ public class TokenReceptionController {
         }
         return null;
     }
+
+    // http://localhost:8080/oauth/token/refresh?refreshToken=
+    @GetMapping(value = "/token/refresh")
+    public AuthToken refreshToken(@RequestParam String refreshToken) throws JsonProcessingException {
+        String credentials = "kstmClientId:kstmClientSecret";
+        String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.add("Authorization", "Basic " + encodedCredentials);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("refresh_token", refreshToken);
+        params.add("grant_type", "refresh_token");
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8080/oauth/token", request, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            AuthToken authToken = this.objectMapper.readValue(response.getBody(), AuthToken.class);
+            log.info(this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(authToken));
+            return authToken;
+        }
+        return null;
+    }
 }
